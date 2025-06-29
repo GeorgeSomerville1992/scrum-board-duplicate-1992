72c7acd0a38569cbcc65cf162629f1df48cb31ab
GeorgeSomerville1992 <georgesomerville1992@gmail.com>
2025-06-29 10:07:51 +0100

    header change

--- src/components/Layout/Header.tsx
+++ src/components/Layout/Header.tsx
@@ -1,7 +1,7 @@
 export const Header = () => (
   <header className="bg-dark-grey w-screen pt-4 pb-4">
     <div className="flex justify-center items-center gap-3">
-      <h1 className="text-3xl font-bold text-gray-900">Scrum Board</h1>
+      <h1 className="text-3xl font-bold text-gray-900">Ideas App</h1>
     </div>
   </header>
 );
